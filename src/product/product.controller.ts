38cf409baf462e0eaaeaf1f113241fecc6acf845
Luis Castro <luisd.ing.pro@gmail.com>
2025-01-13 12:55:39 -0500

    added othrer dependencys

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ProductsService } from './product.service';
 
-@Controller('product')
-export class ProductController {}
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  getAllProducts(
+    @Query('page') page = 1,
+    @Query('category') category?: string,
+    @Query('priceMin') priceMin?: number,
+    @Query('priceMax') priceMax?: number,
+  ) {
+    return this.productsService.getProducts({ page, category, priceMin, priceMax });
+  }
+}
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from '../database/entities/product.entity';
 
 @Injectable()
-export class ProductService {}
+export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async getProducts(filters: any) {
+    const { page, category, priceMin, priceMax } = filters;
+    const take = 5;
+    const skip = (page - 1) * take;
+
+    const query = this.productRepository.createQueryBuilder('product').where('product.deleted = :deleted', {
+      deleted: false,
+    });
+
+    if (category) query.andWhere('product.category = :category', { category });
+    if (priceMin) query.andWhere('product.price >= :priceMin', { priceMin });
+    if (priceMax) query.andWhere('product.price <= :priceMax', { priceMax });
+
+    const [data, total] = await query.skip(skip).take(take).getManyAndCount();
+    return { data, total, page };
+  }
+}
