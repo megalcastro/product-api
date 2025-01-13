d899562c4e3b4aa6c6ebfab89a976716a101f5a7
Luis Castro <luisd.ing.pro@gmail.com>
2025-01-13 14:10:19 -0500

    fixed

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,9 +5,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../database/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])], // Registra la entidad
+  imports: [TypeOrmModule.forFeature([User])], 
   controllers: [UsersController],
-  providers: [UsersService], // Registra el servicio
+  providers: [UsersService], 
   exports: [UsersService],
 })
 export class UsersModule {}
